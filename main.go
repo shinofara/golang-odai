bb17a83914535628ac6307573f2ddba075749ce8
shinofara <shinofara+github@gmail.com>
2019-07-15 16:40:05 +0900

    Modify: Graceful Shutdown

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	_ "github.com/go-sql-driver/mysql"
 	"golang-odai/config"
 	"golang-odai/external/http/route"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -24,7 +30,30 @@ func main() {
 		panic(err)
 	}
 
-	if err := http.ListenAndServe(":80", r); err != nil {
-		panic(err)
+	srv := &http.Server{
+		Addr:    ":80",
+		Handler: r,
 	}
+
+	// Graceful Shutdown
+	// SIGTERM発火後動作中のプロセスを即時停止させるのではなく、受付を停止して、処理中のリクエストがなくなるまで起動を続ける。
+	// すべてのリクエストの処理が完了したらHTTPサーバを停止させる。
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			// Error starting or closing listener:
+			log.Fatalln("Server closed with error:", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+	log.Printf("SIGNAL %d received, then shutting down...\n", <-quit)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		// Error from closing listeners, or context timeout:
+		log.Println("Failed to gracefully shutdown:", err)
+	}
+	log.Println("Server shutdown")
 }
